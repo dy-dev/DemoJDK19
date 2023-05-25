0483eac8f0149159fe4b1382daad0e9c38063030
Dominique YOLIN <d.yolin@isartdigital.com>
2023-05-25 11:50:30 +0200

    Demo values, string and resources first approach to Activities and Views

--- app/src/main/java/com/example/demojdk19/MainActivity.java
+++ app/src/main/java/com/example/demojdk19/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.demojdk19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String s = getResources().getString(R.string.hello_world);
+        Color c = Color.valueOf(getResources().getColor(R.color.black, getTheme()));
+
+        TextView t = findViewById(R.id.my_text);
+        t.setText(android.R.string.paste);
+//        text.setText("Toto");
+//        setContentView(text);
+
     }
 }
