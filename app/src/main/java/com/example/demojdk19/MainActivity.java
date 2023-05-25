ac6583adf8994221b398282a75b1509429f08673
Dominique YOLIN <d.yolin@isartdigital.com>
2023-05-25 12:54:07 +0200

    Firdsgt activity manipulation with attributes created in xml files

--- app/src/main/java/com/example/demojdk19/MainActivity.java
+++ app/src/main/java/com/example/demojdk19/MainActivity.java
@@ -16,8 +16,22 @@ public class MainActivity extends AppCompatActivity {
         String s = getResources().getString(R.string.hello_world);
         Color c = Color.valueOf(getResources().getColor(R.color.black, getTheme()));
 
-        TextView t = findViewById(R.id.my_text);
-        t.setText(android.R.string.paste);
+
+        TextView third = findViewById(R.id.thirdText);
+        TextView fourth = findViewById(R.id.fourthText);
+
+        third.setText(R.string.ThirdTextStr);
+
+        String str = getString(R.string.FourthTextStr);
+        str += " truc en plus";
+        fourth.setText(str);
+
+//
+//        TextView t = findViewById(R.id.my_text);
+//        t.setText(android.R.string.paste);
+//
+//        boolean b = getResources().getBoolean(R.bool.TestBool);
+
 //        text.setText("Toto");
 //        setContentView(text);
 
